a6f8c34604438edf048462516cffb54c432f5891
ashikagupta33 <omprakashgupta814154@gmail.com>
2025-10-02 18:57:21 +0530

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,17 @@
+import requests
+
+def get_weather(city):
+    url = f"https://wttr.in/{city}?format=3"
+    try:
+        res = requests.get(url)
+        if res.status_code == 200:
+            print(f"Weather in {city}: {res.text}")
+        else:
+            print("Failed to get weather info.")
+    except Exception as e:
+        print("Error:", e)
+
+if __name__ == "__main__":
+    print("Terminal Weather App")
+    city = input("Enter city name: ")
+    get_weather(city)
